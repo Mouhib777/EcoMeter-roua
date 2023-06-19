64a5e861863b89c45cb59c87bf777f4d79e607a4
Mouhib777 <mouhibsahbani@gmail.com>
2023-06-19 15:34:23 +0000

    cc

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -4,5 +4,5 @@ final urlGoogleSheet =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGCuVBWtb6OQHT_ObsklWUt6Ft8e89HKh60nuFSH0BeGdOVNN9zq0_QNKfQmui0pDmmFfIZrjnDe-Z/pub?output=csv";
 final url2GoogleSheet =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGCuVBWtb6OQHT_ObsklWUt6Ft8e89HKh60nuFSH0BeGdOVNN9zq0_QNKfQmui0pDmmFfIZrjnDe-Z/pub?gid=1286624421&single=true&output=csv";
-const primaryColor = Color(0xFF4FAF5A);
-const secondaryColor = Color(0xFFC6C6C6);
+const primaryColor = Color(0xFF19278a);
+const secondaryColor = Color(0xffa2061b);
