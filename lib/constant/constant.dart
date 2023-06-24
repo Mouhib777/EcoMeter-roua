eca05c875dd64a1a369ab2a845e151a10dc70504
Mouhib777 <mouhibsahbani@gmail.com>
2023-06-24 14:14:30 +0000

    commit

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 final urlGoogleSheet =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGCuVBWtb6OQHT_ObsklWUt6Ft8e89HKh60nuFSH0BeGdOVNN9zq0_QNKfQmui0pDmmFfIZrjnDe-Z/pub?output=csv";
 final url2GoogleSheet =
-    "https://docs.google.com/spreadsheets/d/1NpfOCyPph0jYjbnSc5PZBPWyYO-P6cf1-4v998JrLw4/edit#gid=1286624421";
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGCuVBWtb6OQHT_ObsklWUt6Ft8e89HKh60nuFSH0BeGdOVNN9zq0_QNKfQmui0pDmmFfIZrjnDe-Z/pub?gid=1286624421&single=true&output=csv";
 const primaryColor = Color(0xFF19278a);
 const secondaryColor = Color(0xffa2061b);
